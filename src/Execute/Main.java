b3d0a8d33d6287d3be9be041d9c581b07642b9fc
Dustin Jia <dustin.jia@nio.io>
2018-05-18 16:20:14 +0800

    Problem 582

--- src/Execute/Main.java
+++ src/Execute/Main.java
@@ -3,6 +3,13 @@ package Execute;
 public class Main {
 
     public static void main(String[] args) {
+        int len = 2;
+        String s = "abcd";
+        String s1 = s.substring(0, len);
+        String s2 = s.substring(len);
 
+        System.out.println(s);
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
---
+++ src/LintCode/WordBreak2_582.java
@@ -0,0 +1,55 @@
+package LintCode;
+
+// https://www.lintcode.com/problem/word-break-ii/description
+
+/**
+ * Given a string s and a dictionary of words dict,
+ * add spaces in s to construct a sentence where each word is a valid dictionary word.
+ */
+
+/**
+ * Example
+ * Gieve s = lintcode, dict = ["de", "ding", "co", "code", "lint"].
+ * A solution is ["lint code", "lint co de"].
+ */
+
+import java.util.*;
+
+public class WordBreak2_582 {
+
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        Map<String, LinkedList<String>> memory = new HashMap<>();
+        return dfs(s, wordDict, memory);
+    }
+
+    private List<String> dfs(String s, Set<String> wordDict, Map<String, LinkedList<String>> memory) {
+        if (memory.containsKey(s)) {
+            return memory.get(s);
+        }
+
+        LinkedList<String> result = new LinkedList<>();
+
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+
+        if (wordDict.contains(s)) {
+            result.add(s);
+        }
+
+        for (int i = 1; i < s.length(); i++) {
+            String word = s.substring(0, i);
+
+            if (wordDict.contains(word)) {
+                String leftover = s.substring(i);
+                List<String> segmentation = dfs(leftover, wordDict, memory);
+                for (String segment : segmentation) {
+                    result.add(word + " " + segment);
+                }
+            }
+        }
+
+        memory.put(s, result);
+        return result;
+    }
+}
