0fcae24c7dbd6d9edb54c584fabe333ffe43b43d
Dustin Jia <dustin.jia@nio.io>
2018-05-31 00:04:55 +0800

    Problem 197

---
+++ src/LintCode/PermutationIndex_197.java
@@ -0,0 +1,36 @@
+package LintCode;
+
+// https://www.lintcode.com/problem/permutation-index/description
+
+/**
+ * Given a permutation which contains no repeated number, find its index in all the permutations of these numbers,
+ * which are ordered in lexicographical order.
+ * The index begins at 1.
+ */
+
+/**
+ * Example
+ *
+ * Given [1,2,4], return 1.
+ */
+
+public class PermutationIndex_197 {
+
+    public long permutationIndex(int[] A) {
+        long permutation = 1;
+        long result = 0;
+
+        for (int i = A.length - 2; i >= 0; i--) {
+            int smaller = 0;
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[j] < A[i]) {
+                    smaller++;
+                }
+            }
+            result += smaller * permutation;
+            permutation *= A.length - i;
+        }
+
+        return result + 1;
+    }
+}
--- src/LintCode/Permutations2_16.java
+++ src/LintCode/Permutations2_16.java
@@ -60,4 +60,3 @@ public class Permutations2_16 {
         }
     }
 }
-
