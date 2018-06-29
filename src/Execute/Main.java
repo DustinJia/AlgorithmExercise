4ab47c65c4a7d69eaf60f605350e8e2a612aacab
Dustin Jia <dustin.jia@nio.io>
2018-06-29 14:21:12 -0700

    Problem 495

--- src/Execute/Main.java
+++ src/Execute/Main.java
@@ -1,9 +1,17 @@
 package Execute;
 
 
+import LintCode.WordPattern2_829;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Map<String, ArrayList<String>> nextWordsDict = new HashMap<>();
+        ArrayList<String> result = nextWordsDict.get("a");
+        System.out.println(result);
     }
 }
---
+++ src/LintCode/ImplementStack_495.java
@@ -0,0 +1,58 @@
+package LintCode;
+
+// https://www.lintcode.com/problem/implement-stack/description
+
+/**
+ * Implement a stack. You can use any data structure inside a stack except stack itself to implement it.
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Example
+ * push(1)
+ * pop()
+ * push(2)
+ * top()  // return 2
+ * pop()
+ * isEmpty() // return true
+ * push(3)
+ * isEmpty() // return false
+ */
+
+public class ImplementStack_495 {
+
+    private ArrayList<Integer> stackArray = new ArrayList<>();
+
+    /*
+     * @param x: An integer
+     * @return: nothing
+     */
+    public void push(int x) {
+        stackArray.add(x);
+    }
+
+    /*
+     * @return: nothing
+     */
+    public void pop() {
+        if (!stackArray.isEmpty()) {
+            stackArray.remove(stackArray.size() - 1);
+        }
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int top() {
+        return stackArray.get(stackArray.size() - 1);
+    }
+
+    /*
+     * @return: True if the stack is empty
+     */
+    public boolean isEmpty() {
+        return stackArray.isEmpty();
+    }
+}
