b948dfdb76e1b09b1062bb2bc8a0ff9a8dab192a
Dustin Jia <dustin.jia@nio.io>
2018-07-12 17:12:33 -0700

    Problem 44

--- src/LintCode/MinimumSubarray_44.java
+++ src/LintCode/MinimumSubarray_44.java
@@ -1,7 +1,37 @@
 package LintCode;
 
+// https://www.lintcode.com/problem/minimum-subarray/description
+
 /**
- * Created by dustin.jia on 7/12/18.
+ * Given an array of integers, find the subarray with smallest sum. Return the sum of the subarray.
+ * Notice
+ *   The subarray should contain at least one number.
  */
+
+/**
+ * Example
+ * For [1, -1, -2, 1], return -3.
+ */
+
+import java.util.List;
+
 public class MinimumSubarray_44 {
+
+    public int minSubArray(List<Integer> nums) {
+        if (nums == null || nums.isEmpty()) {
+            return 0;
+        }
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        int maxSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums.get(i);
+            min = Math.min(min, sum - maxSum);
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return min;
+    }
 }
