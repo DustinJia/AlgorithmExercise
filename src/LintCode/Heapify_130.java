fc84637456c46b8c7b65ed8625020d53cbcd18a4
Dustin Jia <dustin.jia@nio.io>
2018-07-12 17:11:54 -0700

    Problem 130

---
+++ src/LintCode/Heapify_130.java
@@ -0,0 +1,21 @@
+package LintCode;
+
+// https://www.lintcode.com/problem/ugly-number-ii/description
+
+/**
+ * Given an integer array, heapify it into a min-heap array.
+ * For a heap array A, A[0] is the root of heap, and for each A[i],
+ * A[i * 2 + 1] is the left child of A[i] and A[i * 2 + 2] is the right child of A[i].
+ */
+
+/**
+ * Example
+ * If n=9, return 10.
+ */
+
+public class Heapify_130 {
+
+    public void heapify(int[] A) {
+        // write your code here
+    }
+}
---
+++ src/LintCode/MinimumSubarray_44.java
@@ -0,0 +1,7 @@
+package LintCode;
+
+/**
+ * Created by dustin.jia on 7/12/18.
+ */
+public class MinimumSubarray_44 {
+}
